4af99d80033989a6e722c9e8dcab70cd02a15405
Hafiz1st <hafiz100st@gmail.com>
2020-04-14 12:19:32 +0800

    Percabangan switch 02

--- src/percabangan/Switch.java
+++ src/percabangan/Switch.java
@@ -2,22 +2,24 @@ package percabangan;
 
 public class Switch {
     public static void main(String[] args) {
-        int jalanJalan=14;
-        switch (jalanJalan) {
-            case 1:
-                System.out.println("urang lapas");
+        int nilai='C';
+        switch (nilai) {
+            case 'A':
+                System.out.println("Mhs     : \"Terima kasih pak\"");
+                System.out.println("Dosen   : \"Selamat ya !! OwO \"");
                 break;
-            case 2:
-                System.out.println("urang acayan");
+            case 'B':
+                System.out.println("Mhs     : \"Kenapa ulun kada dapat A pak?\"");
+                System.out.println("Dosen   : \"Cacapan asam kamal nyaman.\"");
                 break;
-            case 3:
-                System.out.println("ubat nyamuk");
-                break;
-            case 4:
-                System.out.println("main pokego");
+            case 'C':
+                System.out.println("Mhs     : \"Ulun turun pul Pak ai, tugas pul jua\"");
+                System.out.println("Dosen   : \"Tapi bisa lah manjawab ujian?\"");
+                System.out.println("Mhs     : \"Dame desu sensei san. Gommen-ne~~~ (T_T) \"");
                 break;
             default:
-                System.out.println("kana corona");
+                System.out.println("Mhs     : \"Terima kasih pak, saya tidak lulus\"");
+                System.out.println("Dosen   : \"Selamat ya atas ketidaklulusannya\"");
                 break;
         }
     }
